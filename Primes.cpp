571431f696f491d4ea494eee76510168e0d5d279
lefrostx <lefrostx@gmail.com>
2016-08-07 01:06:43 +0500

    Добавлена реализация функции isPrimeEach1, подкорректирована функция isPrimeEach0

--- Primes.cpp
+++ Primes.cpp
@@ -2,7 +2,24 @@
 
 bool Primes::isPrimeEach0(long number)
 {
-    for (long i{1L}; i <= number; ++i)
+    if (number == 2)
+        return true;
+
+    for (long i{2L}; i < number; ++i)
+		if (number % i == 0)
+			return false;
+	return true;
+}
+
+bool Primes::isPrimeEach1(long number)
+{
+    if (number == 2L)
+        return true;
+    
+    if (number % 2L == 0)
+        return false;
+
+    for (long i{3L}; i < number; i += 2)
 		if (number % i == 0)
 			return false;
 	return true;
