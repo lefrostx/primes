c906cfafb34c8ebbcd8c9fe58dca15e4b2079491
lefrostx <lefrostx@gmail.com>
2016-08-07 01:05:44 +0500

    Добавлен тест для функции isPrimeEach1

--- PrimesUnitTest.cpp
+++ PrimesUnitTest.cpp
@@ -11,14 +11,23 @@ auto testRange = bdata::xrange(1L, endPrimeRange, 1L);
 
 bool isPrime(long number)
 {
-	for (long i{1L}; i <= number; ++i)
+    if (number == 2)
+        return true;
+
+    for (long i{2L}; i < number; ++i)
 		if (number % i == 0)
 			return false;
 	return true;
 }
 
-BOOST_DATA_TEST_CASE(testIsPrimeEach0, testRange)
+BOOST_DATA_TEST_CASE(testIsPrimeEach0, testRange, number)
 {
-	bool result{ isPrime(sample) == Primes::isPrimeEach0(sample) };
+	bool result{ isPrime(number) == Primes::isPrimeEach0(number) };
+	BOOST_TEST(result);
+}
+
+BOOST_DATA_TEST_CASE(testIsPrimeEach1, testRange, number)
+{
+	bool result{ isPrime(number) == Primes::isPrimeEach1(number) };
 	BOOST_TEST(result);
 }
