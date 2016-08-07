4f175989a707d4f1ecf831ce74e7a7f8a78d5b0b
lefrostx <lefrostx@gmail.com>
2016-08-07 13:28:11 +0500

    Обновлена система объявлений функций

--- Primes.h
+++ Primes.h
@@ -1,7 +1,8 @@
 namespace Primes {
     bool isPrimeEach0(long number);
     bool isPrimeEach1(long number);
-    bool isPrimeHalf0(long number);
+    bool isPrimeEach2(long number);
     bool isPrimeHalf1(long number);
-    bool isPrimeSqrt0(long number);
+    bool isPrimeHalf2(long number);
+    bool isPrimeSqrt1(long number);
 }
